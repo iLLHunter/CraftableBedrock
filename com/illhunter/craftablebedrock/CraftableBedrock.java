9acb369d3908a8c920bdacf04906d0227a5c20fe
iLLHunter <iLLHunterMC@gmail.com>
2014-03-28 10:21:43 -0400

    removed commets added from tutorial. haha n00b

--- com/illhunter/craftablebedrock/CraftableBedrock.java
+++ com/illhunter/craftablebedrock/CraftableBedrock.java
@@ -1,16 +1,12 @@
 package com.illhunter.craftablebedrock;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler; // used in 1.6.2
-//import cpw.mods.fml.common.Mod.PreInit;    // used in 1.5.2
-//import cpw.mods.fml.common.Mod.Init;       // used in 1.5.2
-//import cpw.mods.fml.common.Mod.PostInit;   // used in 1.5.2
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-//import cpw.mods.fml.common.network.NetworkMod; // not used in 1.7
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -24,7 +20,6 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid="CraftableBedrock", name="Craftable Bedrock", version="0.1.0")
 public class CraftableBedrock {
-	// The instance of your mod that Forge uses.
     @Instance(value = "CraftableBedrock")
     public static CraftableBedrock instance;
     
@@ -32,18 +27,15 @@ public class CraftableBedrock {
     
     public static final Block[] blocksBPEffectiveAgainst = new Block[] {Block.bedrock};
     
-    // Says where the client and server 'proxy' code is loaded.
     @SidedProxy(clientSide="com.illhunter.craftablebedrock.client.ClientProxy", serverSide="com.illhunter.craftablebedrock.CommonProxy")
     public static CommonProxy proxy;
     
-    @EventHandler // used in 1.6.2
-    //@PreInit    // used in 1.5.2
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
     			bedrockPickaxe = new BedrockPickaxe(13613, 4.0F, EnumToolMaterial.STONE, blocksBPEffectiveAgainst);
     }
     
-    @EventHandler // used in 1.6.2
-    //@Init       // used in 1.5.2
+    @EventHandler
     public void load(FMLInitializationEvent event) {
             proxy.registerRenderers();
           //Craftable Bedrock
@@ -64,9 +56,8 @@ public class CraftableBedrock {
 
 }
     
-    @EventHandler // used in 1.6.2
-    //@PostInit   // used in 1.5.2
+    @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-            // Stub Method
+            // Stubitz
     }
 }
